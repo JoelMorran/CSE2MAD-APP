40499236c7be27d3a3e7198d4b95a4ac6476eb33
Jamorran <joel911@live.com.au>
2018-09-14 19:33:18 +1000

    First Commit

---
+++ app/src/main/java/com/example/kobiqoi_laptop/assignment/Checkout2Activity.java
@@ -0,0 +1,90 @@
+package com.example.kobiqoi_laptop.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Checkout2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checkout2);
+
+        //signIn = (Button) findViewById(R.id.signIn);
+        //signUp = (Button) findViewById(R.id.signUp);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.mytoolbar);
+        setSupportActionBar(myToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        // ActionBar ab = getSupportActionBar();//this works with onSupportNavigateUp()
+
+        // Enable the Up button
+        //  ab.setDisplayHomeAsUpEnabled(true);//this works with onSupportNavigateUp()
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//this works with onSupportNavigateUp()
+        getSupportActionBar().setDisplayShowHomeEnabled(true); //this works with onSupportNavigateUp()
+
+       /* signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(SignInSignUpActivity.this, LoginPageActivity.class);
+
+                SignInSignUpActivity.this.startActivity(myIntent);
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(SignInSignUpActivity.this, CreateNewAccountActivity.class);
+
+                SignInSignUpActivity.this.startActivity(myIntent);
+            }
+        });*/
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_back:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menubuttons, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        // Configure the search info and add any event listeners...
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/kobiqoi_laptop/assignment/LetsEatActivity.java
@@ -0,0 +1,30 @@
+package com.example.kobiqoi_laptop.assignment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LetsEatActivity extends AppCompatActivity {
+
+    private Button LetsEat = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_lets_eat);
+        LetsEat = (Button) findViewById(R.id.LetsEat);
+
+
+        LetsEat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(LetsEatActivity.this, SignInSignUpActivity.class);
+               // myIntent.putExtra("contact_num", contact_number);
+                LetsEatActivity.this.startActivity(myIntent);
+                }
+        });
+    }
+}
+
